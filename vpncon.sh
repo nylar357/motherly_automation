df4ab32f4cae267423e09b8280f8cca4e0f3467d
Bryce DaVarry <37067686+nylar357@users.noreply.github.com>
2021-09-18 02:08:06 -0400

    Update vpncon.sh

--- vpncon.sh
+++ vpncon.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 shorty=$(xfce4-terminal --fullscreen --zoom=1 --hold --show-scrollbar --color-text=#F8E4A8 --color-bg=#6e7b8b --font=14 -x cat /home/user1/documents/shortcuts.txt)
-echo "Connect : ./vpncon.sh -1 | Disconnect : ./vpncon.sh -2 | Select USA @ bottom, Select State, Chose UDP" 
+echo "|Connect : VPN Connect -1 | Disconnect : VPN Disconnect -2 | Keyboard Shortcuts Displayed -3 |"
 sleep 3s &&
 echo
 while [ -n "$1" ]
